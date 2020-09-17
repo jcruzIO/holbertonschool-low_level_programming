d5674d63278df03ac8263ab2d2e6b770ff30ce24
jcruzIO <jncruz06@gmail.com>
2020-09-17 16:33:46 +0000

    Place all modulo expressions within its own variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,17 +11,19 @@
 int main(void)
 {
 	int n;
+	int n2;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	n2 = n % 10;
 
-	printf("Last digit of %d is %d", n, n % 10);
+	printf("Last digit of %d is %d", n, n2);
 
-	if ((n % 10) > 5)
+	if (n2 > 5)
 		printf("%s", " and is greater than 5\n");
-	else if ((n % 10) == 0)
+	else if (n2 == 0)
 		printf("%s", " and is 0\n");
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if (n2 < 6 && n2 != 0)
 		printf("%s", "%d and is less than 6 and not 0\n");
 
 	return (0);
