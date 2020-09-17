74f75550513a8023ed6eef97186e9c51380e9f45
jcruzIO <jncruz06@gmail.com>
2020-09-17 16:42:49 +0000

    Remove %d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	else if (n2 == 0)
 		printf("%s", " and is 0\n");
 	else if (n2 < 6 && n2 != 0)
-		printf("%s", "%d and is less than 6 and not 0\n");
+		printf("%s", " and is less than 6 and not 0\n");
 
 	return (0);
 }
