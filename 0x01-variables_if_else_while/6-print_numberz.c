753724b3e21c46ac18d930fae0cb9703b8785bc3
jcruzIO <jncruz06@gmail.com>
2020-09-17 18:39:22 +0000

    Replaced object file with .c file to repo

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+/**
+* main - This program prints all single digit numbers
+* of base 10 starting from 0 with exception that
+* no variables of type char can be declared nor
+* using putchar more than twice
+*
+* Return: 0 to stop program
+*/
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	putchar('\n');
+
+	return (0);
+}
