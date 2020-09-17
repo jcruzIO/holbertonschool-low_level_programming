0b0bfdf1be9cb596d2572035518b3475c21b6d5e
jcruzIO <jncruz06@gmail.com>
2020-09-17 01:12:53 +0000

    Remove trailing whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
 * main - Prints statement
 *
-* Return: 0 to exit out of funcction
+* Return: 0 to exit out of function
 */
 int main(void)
 {
-char *str = "\"Programming is like building a multilingual puzzle";   
-puts(str);
-return (0); 
+	char *str = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
 }
