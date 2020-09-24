de090ef139fff46e8c2d5d81f4b8a15d75db2b1d
jcruzIO <jncruz06@gmail.com>
2020-09-24 15:32:21 +0000

    Add file 4 with answer

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - This function prvoids the numbers 0 to 9
+ * while skipping 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
