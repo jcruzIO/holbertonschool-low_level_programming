4ce1f66e8ebb5fdae0b817a057fc39b8a112dcd5
jcruzIO <jncruz06@gmail.com>
2020-09-24 01:43:26 +0000

    Add file 102 with answer

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Program that computes the first 50 Fibonacci numbers
+ *
+ * Return: 0 to stop program
+ */
+
+int main(void)
+{
+	int i;
+	int first = 1;
+	int second = 2;
+	unsigned long long int sum = 0;
+
+	printf("%d, %d, ", first, second);
+
+	for (i = 0; i < 48; i++)
+	{
+		sum = first + second;
+		second = first;
+		first = sum;
+		printf("%llu, ", sum);
+	}
+	printf("\n");
+}
