29a24b834634f439b816e7ebaabc6bfe14b4b854
jcruzIO <jncruz06@gmail.com>
2020-09-25 03:05:45 +0000

    More rigorous testing of conditionals using nesting

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,19 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			if (i % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("Fizz Buzz");
+		{
+			if (i % 3 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Buzz ");
+		}
 		else
 			printf("%d ", i);
 	}
