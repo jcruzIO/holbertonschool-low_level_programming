65842ce08a4269c4866fd652a5b221e1ae4728d8
jcruzIO <jncruz06@gmail.com>
2020-09-25 18:18:59 +0000

    Add file 101 with answer + updated header file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_number - This function prints an integer
+ * @n: The integer to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n < 10)
+		_putchar(n + '0');
+	else
+	{
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+}
