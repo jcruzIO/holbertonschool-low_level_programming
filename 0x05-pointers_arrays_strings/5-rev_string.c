ef299ef4350ea1801a99b5f161f21073a2c41791
jcruzIO <jncruz06@gmail.com>
2020-09-30 02:46:40 +0000

    Update answer for file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,19 @@ void rev_string(char *s)
 {
 	int size = sizeof(s) / sizeof(s[0]);
 	int i;
+	int j = 0;
+	char s2[10];
 
-	for (i = size; i >= 0; i--)
+	for (i = size; i >= 0; i--, j++)
 	{
-		_putchar(s[i]);
+		s2[j] = s[i];
 	}
-	_putchar('\n');
+
+	size = sizeof(s2) / sizeof(s2[0]);
+
+	for (i = 0; i < size - 1; i++)
+	{
+		s[i] = s2[i];
+	}
+
 }
