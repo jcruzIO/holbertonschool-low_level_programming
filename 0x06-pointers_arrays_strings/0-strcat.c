96d986cc058dd2ea167f39600ac2c34973b34e1b
jcruzIO <jncruz06@gmail.com>
2020-10-01 16:01:15 +0000

    Add file 0 with answer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcat - This function concatenates two strings
+ * @dest: Place where the src is copied to
+ * @src: Text to be copied
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0';)
+		i++;
+
+	for (j = 0; (dest[i++] = src[j++]) != '\0';);
+
+	return (dest);
+}
